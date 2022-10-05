40fd92f5ae890628d2c173337053fce453a70fb8
Kosmodesant <90253666+Kosmodesant@users.noreply.github.com>
2022-10-05 19:58:41 +0300

    Update resizePhotos.py

--- resizePhoto/resizePhotos.py
+++ resizePhoto/resizePhotos.py
@@ -12,3 +12,9 @@ new_im.save("example_resized.jpg")
 # im = Image.open("example.jpg")
 # im.rotate(180).resize((640,480)).save("flipped_and_resized.jpg")
 
+# import os
+# import glob
+# from PIL import Image
+# for x in glob.glob("ic*"):
+# 	mod_image = Image.open(x).convert("RGB")
+# 	mod_image.rotate(270).resize((128,128)).save("/opt/icons/" + x,"jpg")
