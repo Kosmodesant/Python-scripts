e4f4fa2fdfbf145b8afb07ec519f999b19fa124e
Kosmodesant <90253666+Kosmodesant@users.noreply.github.com>
2022-10-17 23:11:28 +0300

    Update movies-query-operators.py

--- MongoDB/movies-query-operators.py
+++ MongoDB/movies-query-operators.py
@@ -1,28 +1,28 @@
 import pymongo
 import pprint
 
-//Replace XXXX with your connection URI from the Atlas UI
+# Replace XXXX with your connection URI from the Atlas UI
 mc = pymongo.MongoClient("XXXX")
 mflix = mc['mflix']
 
-//find all movies released from 1983 onwards
+# find all movies released from 1983 onwards
 filters = {'year': { '$gte': 1983 }}
 
 for movie in mflix.movies.find(filters):
     pprint.pprint(movie['title'])
 
-//find all movies between 1989 and 1999
+# //find all movies between 1989 and 1999
 filters = { 'year': {'$gte': 1989, '$lt': 2000} }
 
 for movie in mflix.movies.find(filters):
     pprint.pprint(movie['title'])
 
-//find all movies released in 1995, 2005, 2015
+# find all movies released in 1995, 2005, 2015
 filters = { 'year': { '$in': [ 1995, 2005, 2015 ] } }
 for movie in mflix.movies.find(filters):
     pprint.pprint(movie['title'])
 
-//find all movies except the ones which are of adult genre
+# find all movies except the ones which are of adult genre
 filters = { 'year': { '$in': [ 1995, 2005, 2015 ] }, 
                      'genre': { '$not' : {'$eq': 'Adult'} } }
 
